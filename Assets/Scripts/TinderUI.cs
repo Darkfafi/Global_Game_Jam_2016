d13a2037862feb662898a00f1d362fb201c19b17
Simon Alkemade <simon@mobytron.com>
2016-01-30 00:32:24 +0100

    fix scenemanager problem

--- Assets/Scripts/TinderUI.cs
+++ Assets/Scripts/TinderUI.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+//using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,77 +7,76 @@ using DG.Tweening;
 
 public class TinderUI : MonoBehaviour
 {
-    public List<LevelName> levels = new List<LevelName>();
-    public Button likeButton;
-    public Button dislikeButton;
+	public List<LevelName> levels = new List<LevelName> ();
+	public Button likeButton;
+	public Button dislikeButton;
 
-    private Vector3 pos;
-    private Quaternion rot;
+	private Vector3 pos;
+	private Quaternion rot;
 
 
-    private int _indexToSwap = 3;
+	private int _indexToSwap = 3;
 
-    private Sequence likeSequence;
-    private Sequence dislikeSequence;
+	private Sequence likeSequence;
+	private Sequence dislikeSequence;
 
-    // Use this for initialization
-    void Start()
-    {
-		likeButton.onClick.AddListener(() => Liked());
-		dislikeButton.onClick.AddListener(() => Disliked());
+	// Use this for initialization
+	void Start ()
+	{
+		likeButton.onClick.AddListener (() => Liked ());
+		dislikeButton.onClick.AddListener (() => Disliked ());
 
-        pos = levels[_indexToSwap].transform.position;
-        rot = levels[_indexToSwap].transform.rotation;
+		pos = levels [_indexToSwap].transform.position;
+		rot = levels [_indexToSwap].transform.rotation;
 		//todo make work
-    }
+	}
 
-	private void Liked()
+	private void Liked ()
 	{
-		DOTween.CompleteAll(true);
+		DOTween.CompleteAll (true);
 
-        levels[_indexToSwap].transform.DOMoveX(16, 0.5f).SetEase(Ease.InSine).OnComplete(LikeCompleted);
-        levels[_indexToSwap].transform.DOMoveY(10, 0.4f).SetEase(Ease.InCirc);
-        levels[_indexToSwap].transform.DORotate(new Vector3(0, 0, 40), 0.45f);
+		levels [_indexToSwap].transform.DOMoveX (16, 0.5f).SetEase (Ease.InSine).OnComplete (LikeCompleted);
+		levels [_indexToSwap].transform.DOMoveY (10, 0.4f).SetEase (Ease.InCirc);
+		levels [_indexToSwap].transform.DORotate (new Vector3 (0, 0, 40), 0.45f);
 
-        // choose this level
-    }
+		// choose this level
+	}
 
-	private void LikeCompleted()
+	private void LikeCompleted ()
 	{
-		print("load level: "+levels[_indexToSwap].levelName);
-        SceneManager.LoadScene(levels[_indexToSwap].levelName);
+		print ("load level: " + levels [_indexToSwap].levelName);
+		Application.LoadLevel (levels [_indexToSwap].levelName);
 
-        levels[_indexToSwap].transform.position = pos;
-        levels[_indexToSwap].transform.rotation = rot;
-    }
+		levels [_indexToSwap].transform.position = pos;
+		levels [_indexToSwap].transform.rotation = rot;
+	}
 
-	private void Disliked()
+	private void Disliked ()
 	{
-		print("disliked");
+		print ("disliked");
 
-		DOTween.CompleteAll(true);
+		DOTween.CompleteAll (true);
 
-        levels[_indexToSwap].transform.DOMoveX(-16, 0.5f).SetEase(Ease.InSine).OnComplete(DislikeCompleted);
-        levels[_indexToSwap].transform.DOMoveY(10, 0.4f).SetEase(Ease.InCirc);
-        levels[_indexToSwap].transform.DORotate(new Vector3(0, 0, -40), 0.45f);
+		levels [_indexToSwap].transform.DOMoveX (-16, 0.5f).SetEase (Ease.InSine).OnComplete (DislikeCompleted);
+		levels [_indexToSwap].transform.DOMoveY (10, 0.4f).SetEase (Ease.InCirc);
+		levels [_indexToSwap].transform.DORotate (new Vector3 (0, 0, -40), 0.45f);
 
 
-        //next frame
-    }
+		//next frame
+	}
 
-	private void DislikeCompleted()
+	private void DislikeCompleted ()
 	{
-        print("dislikecompl");
-        levels[_indexToSwap].transform.SetAsFirstSibling();
+		print ("dislikecompl");
+		levels [_indexToSwap].transform.SetAsFirstSibling ();
 
-        levels[_indexToSwap].transform.position = pos;
-        levels[_indexToSwap].transform.rotation = rot;
+		levels [_indexToSwap].transform.position = pos;
+		levels [_indexToSwap].transform.rotation = rot;
 
 
-        _indexToSwap--;
-		if (_indexToSwap == -1)
-		{
-            _indexToSwap = 3;
-        }
+		_indexToSwap--;
+		if (_indexToSwap == -1) {
+			_indexToSwap = 3;
+		}
 	}
 }
