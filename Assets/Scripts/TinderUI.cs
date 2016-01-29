fd9bb1d8da5a937fa868d1cdab15e82ebd9ed5df
Nick Aarts <Nickabilla@Nicks-MacBook-Pro.local>
2016-01-29 21:51:41 +0100

    Added TinderUI

---
+++ Assets/Scripts/TinderUI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TinderUI : MonoBehaviour
+{
+    public List<LevelName> levels = new List<LevelName>();
+    public Button likeButton;
+    public Button dislikeButton;
+
+    private int _indexToSwap = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+		likeButton.onClick.AddListener(() => Liked());
+		dislikeButton.onClick.AddListener(() => Disliked());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	private void Liked()
+	{
+		print("load level: "+levels[_indexToSwap].levelName);
+
+		// choose this level
+    }
+
+	private void Disliked()
+	{
+		print("disliked");
+        levels[_indexToSwap].transform.SetAsFirstSibling();
+        _indexToSwap--;
+		if (_indexToSwap == -1)
+		{
+            _indexToSwap = 3;
+        }
+
+		//next frame
+	}
+}
