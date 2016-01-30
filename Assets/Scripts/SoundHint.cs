8732b2204666945fdabc0f533069c899ff56afa5
Simon Alkemade <simon@mobytron.com>
2016-01-30 17:46:58 +0100

    working on a sound tip system

---
+++ Assets/Scripts/SoundHint.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundHint : MonoBehaviour
+{
+
+	static public SoundHint Instance;
+
+	public Transform[] hints;
+
+	void Awake ()
+	{
+		if (Instance != null) {
+			Debug.LogError ("SoundHint Already exists");
+		} else {
+			Instance = this;
+		}
+	}
+
+	public void YellowY ()
+	{
+
+	}
+
+	public void GreenA ()
+	{
+		
+	}
+
+	public void RedB ()
+	{
+		
+	}
+
+	public void BlueX ()
+	{
+		
+	}
+
+}
