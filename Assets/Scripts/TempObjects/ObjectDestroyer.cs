9566b2e44c9052ac59195493ea98a3e1d81de22e
Ramses di Perna <15826@ma-web.nl>
2016-01-30 21:11:38 +0100

    Made heart feel

--- Assets/Scripts/TempObjects/ObjectDestroyer.cs
+++ Assets/Scripts/TempObjects/ObjectDestroyer.cs
@@ -5,21 +5,12 @@ public class ObjectDestroyer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Destroy (this.gameObject, 1.5f);
+		//Destroy (this.gameObject, 1.5f);
 	}
 	void Update(){
-		if (transform.localScale.x > 0) {
-			transform.localScale  = new Vector3(Mathf.Lerp(transform.localScale.x,transform.localScale.x - 1,0.1f),transform.localScale.y,transform.localScale.z);
-		}else{
-			transform.localScale = new Vector3(0,transform.localScale.y,transform.localScale.z);;
-		}
-		if (transform.localScale.y > 0) {
-
-			transform.localScale = new Vector3(transform.localScale.x,Mathf.Lerp(transform.localScale.y,transform.localScale.y - 1,0.1f),transform.localScale.z);
-		}else{
-			transform.localScale = new Vector3(transform.localScale.x,0,transform.localScale.z);
-		}
-		if (transform.localScale.x == 0 && transform.localScale.y == 0) {
+		Color color = GetComponent<SpriteRenderer> ().color;
+		GetComponent<SpriteRenderer> ().color = new Color(color.r,color.g,color.b,color.a - (0.8f * Time.deltaTime));
+		if (color.a <= 0) {
 			Destroy(this.gameObject);
 		}
 	}
