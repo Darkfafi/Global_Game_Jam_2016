184824dcf8e33f4ae25528114984ec18e89f6fa6
Nick Aarts <Nickabilla@Nicks-MacBook-Pro.local>
2016-01-30 15:18:05 +0100

    yes

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+    public GameObject splash;
+    public GameObject bg;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
