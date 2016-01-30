7df2fd8865a448d364164696e010ef6b427403f3
Simon Alkemade <simon@mobytron.com>
2016-01-30 21:06:14 +0100

    timer added

--- Assets/Scripts/Characters/SeduceableTarget.cs
+++ Assets/Scripts/Characters/SeduceableTarget.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-public class SeduceableTarget : MonoBehaviour {
+public class SeduceableTarget : MonoBehaviour
+{
 
 	private Character _character;
-	[SerializeField]private Character _targetCharacter;
+	[SerializeField]
+	private Character _targetCharacter;
+	[SerializeField]
+	private Transform _timerTransform;
 	private SeduceList _seduceList;
 	private int _seducePatternIndex = 0;
 
@@ -14,21 +18,23 @@ public class SeduceableTarget : MonoBehaviour {
 
 	private bool _listeningToSeduction = false;
 
-	void Start(){
+	void Start ()
+	{
 		_character = gameObject.GetComponent<Character> ();
 		_seduceList = gameObject.GetComponent<SeduceList> ();
 		_targetCharacter.DidMove += CheckCorrectMove;
 		ChooseSeduction (); //Difficulty has no fucntion yet..
-		StartCoroutine(WaitForListen (3));
+		StartCoroutine (WaitForListen (3));
 	}
 
-	private void ChooseSeduction(bool playRev = false){
+	private void ChooseSeduction (bool playRev = false)
+	{
 		int index = Random.Range (0, PatternLibrary.TOTAL_PATTERNS);
 		if (index == _seducePatternIndex) {
-			if(_seducePatternIndex < PatternLibrary.TOTAL_PATTERNS - 1){
+			if (_seducePatternIndex < PatternLibrary.TOTAL_PATTERNS - 1) {
 				index++;
-			}else{
-				index = Random.Range(0,PatternLibrary.TOTAL_PATTERNS - 1);
+			} else {
+				index = Random.Range (0, PatternLibrary.TOTAL_PATTERNS - 1);
 			}
 		}
 		_seducePatternIndex = index;
@@ -38,37 +44,43 @@ public class SeduceableTarget : MonoBehaviour {
 		}
 	}
 
-	private void PlayReversedAnimation (){
+	private void PlayReversedAnimation ()
+	{
 		for (int i = 0; i < _seduceList.allPartsNeedMoving.Count; i++) {
-			_character.CallPart(_seduceList.allPartsNeedMoving[i]);
-			_character.SetMouth(_seduceList.GetReversedAudio(_seduceList.wantedSoundIndex));
+			_character.CallPart (_seduceList.allPartsNeedMoving [i]);
+			_character.SetMouth (_seduceList.GetReversedAudio (_seduceList.wantedSoundIndex));
 		}
 	}
 
-	private void CheckCorrectMove(){
+	private void CheckCorrectMove ()
+	{
 		if (_seduceList.CheckIfMatch (_targetCharacter)) {
 			_timesSeduces ++;
 			_listeningToSeduction = false;
-			ChooseSeduction();
-			ShowLove();
+			_timerTransform.gameObject.SetActive (false);
+			ChooseSeduction ();
+			ShowLove ();
 		} else {
 			_timesIncorrectGuess++;
-			if(_timesIncorrectGuess == _timesTolorateIncorrect){
-				PlayReversedAnimation();
+			if (_timesIncorrectGuess == _timesTolorateIncorrect) {
+				PlayReversedAnimation ();
 				_timesIncorrectGuess = 0;
-				_timesTolorateIncorrect = Random.Range(3,6);
+				_timesTolorateIncorrect = Random.Range (3, 6);
 			}
 		}
 	}
-	IEnumerator WaitForListen(int seconds = 1){
+	IEnumerator WaitForListen (int seconds = 1)
+	{
 
 		yield return new WaitForSeconds (seconds);
 		PlayReversedAnimation ();
 		_listeningToSeduction = true;
+		_timerTransform.gameObject.SetActive (true);
 	}
-	private void ShowLove(){
+	private void ShowLove ()
+	{
 		//TODO heart above head and show new pattern after x seconds.
 		Instantiate (Resources.Load<GameObject> ("Prefabs/Heart"), new Vector3 (), Quaternion.identity);
-		StartCoroutine(WaitForListen (3));
+		StartCoroutine (WaitForListen (3));
 	}
 }
