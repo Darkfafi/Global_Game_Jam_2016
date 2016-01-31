f84ebbc89b658f4524d67d2fb0c9b4747e284e00
Simon Alkemade <simon@mobytron.com>
2016-01-31 01:42:01 +0100

    cleaned up sound hint although still messy

--- Assets/Scripts/SoundHint.cs
+++ Assets/Scripts/SoundHint.cs
@@ -3,24 +3,11 @@ using System.Collections;
 
 public class SoundHint : MonoBehaviour
 {
-
-
 	public Transform[] ParticleSys;
-	/*
-	static public SoundHint Instance;
 
-	void Awake ()
-	{
-		if (Instance != null) {
-			Debug.LogError ("SoundHint Already exists");
-		} else {
-			Instance = this;
-		}
-	}
-	*/
 	public void YellowY ()
 	{
-		Transform t = Instantiate (ParticleSys [0]) as Transform;//, new Vector3 (0, 2, 0), Quaternion.identity);
+		Transform t = Instantiate (ParticleSys [0]) as Transform;
 		t.position = transform.position;
 		t.Translate (0, 2.5f, -4);
 	}
@@ -30,7 +17,6 @@ public class SoundHint : MonoBehaviour
 		Transform t = Instantiate (ParticleSys [1]) as Transform;
 		t.position = transform.position;
 		t.Translate (0, 2.5f, -4);
-		;//, new Vector3 (0, 2, 0), Quaternion.identity);
 	}
 
 	public void RedB ()
@@ -38,14 +24,14 @@ public class SoundHint : MonoBehaviour
 		Transform t = Instantiate (ParticleSys [2]) as Transform;
 		t.position = transform.position;
 		t.Translate (0, 2.5f, -4);
-		//, new Vector3 (0, 2, 0), Quaternion.identity);
+		;
 	}
 
 	public void BlueX ()
 	{
 		Transform t = Instantiate (ParticleSys [3]) as Transform;
 		t.position = transform.position;
-		t.Translate (0, 2.5f, -4);//, new Vector3 (0, 2, 0), Quaternion.identity);
+		t.Translate (0, 2.5f, -4);
 	}
 
 }
