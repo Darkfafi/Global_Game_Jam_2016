af064114058fb7102f940a854fdd0de555ce497b
Ramses di Perna <15826@ma-web.nl>
2016-01-31 06:51:49 +0100

    Working on sounds and improved movie player system for increased production time

---
+++ Assets/Scripts/Movie/MoviePlayerSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayerSystem : MonoBehaviour {
+	
+	public string movieFolder;
+	private Sprite[] _movie; 
+
+	private SpriteRenderer _renderer;
+	private int _currentSpriteIndex = 0;
+	// Use this for initialization
+	void Start () {
+		_renderer = gameObject.AddComponent<SpriteRenderer> ();
+		_movie = Resources.LoadAll<Sprite> ("Climax/" + movieFolder);
+		StartCoroutine (VideoSystem ());
+	}
+	
+	// Update is called once per frame
+	IEnumerator VideoSystem () {
+		_renderer.sprite = _movie [_currentSpriteIndex];
+		if (_currentSpriteIndex != _movie.Length - 1) {
+			_currentSpriteIndex ++;
+		} else {
+			_currentSpriteIndex = 0;
+		}
+		yield return new WaitForSeconds (.03f);
+		StartCoroutine (VideoSystem ());
+	}
+}
