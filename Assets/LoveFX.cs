11bf9bcb055e75cf06aeedf660597cdb92211cd1
Simon Alkemade <simon@mobytron.com>
2016-01-31 02:02:17 +0100

    LoveFX aan de buitenkant van het level

---
+++ Assets/LoveFX.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoveFX : MonoBehaviour
+{
+
+	public Transform otherLoveFX;
+
+	void Start ()
+	{
+		StartCoroutine (WaitToStartLoveFX ());
+	}
+
+	IEnumerator WaitToStartLoveFX ()
+	{
+		yield return new WaitForSeconds (.5f);
+
+		otherLoveFX.gameObject.SetActive (true);
+	}
+
+}
