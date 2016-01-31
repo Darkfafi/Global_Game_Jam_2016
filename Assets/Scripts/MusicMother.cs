2599dd0856a08fec6a13e3231d0d17a11e8275ee
Nick Aarts <Nickabilla@Nicks-MacBook-Pro.local>
2016-01-31 04:40:40 +0100

    juice jice

---
+++ Assets/Scripts/MusicMother.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicMother : MonoBehaviour {
+    public AudioClip musicTheme;
+    public AudioClip musicMating;
+
+	public static MusicMother Instance { get; private set; }
+
+    // Initialisation
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(this.gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+	public void PlayTheme()
+	{
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().clip = musicTheme;
+        GetComponent<AudioSource>().Play();
+    }
+
+	public void PlayMating()
+	{
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().clip = musicMating;
+        GetComponent<AudioSource>().Play();
+	}
+}
