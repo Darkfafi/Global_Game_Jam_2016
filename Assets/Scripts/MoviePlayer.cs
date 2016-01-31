da9ec0a8093603da019354fcdc5f5c2f7b91af17
Simon Alkemade <simon@mobytron.com>
2016-01-31 05:12:57 +0100

    movie

---
+++ Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoviePlayer : MonoBehaviour
+{
+	public List<Sprite> spriteList = new List<Sprite> ();
+
+	public Sprite goalSprite;
+
+	public	int videoCounter = 0;
+
+	void Start ()
+	{
+		StartCoroutine (VideoSystem ());
+	}
+
+	IEnumerator VideoSystem ()
+	{
+		GetComponent<SpriteRenderer> ().sprite = spriteList [videoCounter];
+
+		if (videoCounter <= 88) {
+			videoCounter++;
+		} else {
+			videoCounter = 0;
+		}
+		yield return new WaitForSeconds (.03f);
+		StartCoroutine (VideoSystem ());
+	}
+	
+}
